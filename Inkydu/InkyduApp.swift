957e75c9fb55aba5aebff9a15eab85c6d9e1177d
Riley Fisher <ref00006@mix.wvu.edu>
2026-04-19 15:39:34 -0400

    Initial Commit

---
+++ Inkydu/InkyduApp.swift
@@ -0,0 +1,17 @@
+//
+//  InkyduApp.swift
+//  Inkydu
+//
+//  Created by Riley Fisher on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct InkyduApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
